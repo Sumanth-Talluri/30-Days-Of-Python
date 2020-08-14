3cce2e1922569d42a41565361a61f22d3aadc78d
SumanthTalluri <tallurisaisumanth77@gmail.com>
2020-08-14 12:25:41 +0530

    added day2

---
+++ day_2/variables.py
@@ -0,0 +1,47 @@
+# Day 2: 30 Days of python programming
+
+first_name = "Sumanth"
+last_name = "Talluri"
+full_name = "Sumanth Talluri"
+country = "India"
+city = "Hyderabad"
+age = "15"
+year = "2004"
+is_married, is_true, is_light_on = False, True, False
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+print(len(first_name))
+
+print(len(last_name))
+
+num_one = 5
+num_two = 4
+
+_total = num_one + num_two
+_diff = num_one - num_two
+_product = num_one * num_two
+_division = num_one / num_two
+_remainder = num_one % num_two
+_exp = num_one ** num_two
+_floor_division = num_one // num_two
+
+radius = 30
+area_of_circle = 3.14*(radius**2)
+
+r = float(input("Enter the radius"))
+area = 3.14*(r**2)
+
+firstname = input("Enter firstname ")
+lastname = input("Enter lastname ")
+country = input("Enter country ")
+age = input("Enter age")
