7066d452cd64b815bd825bef20db04c9f225e71c
Sumanth <tallurisaisumanth77@gmail.com>
2020-08-29 13:44:07 +0000

    added day26

---
+++ Day 26 Python for web/web.py
@@ -0,0 +1,7 @@
+# FLASK
+# You can find the application I built using Flask in the repository 'Readers-Cabin'
+# link : https://github.com/Sumanth-Talluri/Readers-Cabin
+
+# DJANGO
+# You can find the application I built using Django in the repository 'CS50w-Commerce'
+# link : https://github.com/Sumanth-Talluri/CS50w-Commerce
